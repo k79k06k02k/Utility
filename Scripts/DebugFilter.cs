8af284a905e90a645717b6b5634d57e3aefbc518
Arkai <Arkai@randomidea.com>
2016-11-14 21:27:38 +0800

    - Script refactor

--- Scripts/DebugFilter.cs
+++ Scripts/DebugFilter.cs
@@ -3,51 +3,68 @@
 // FileName : DebugFilter.cs
 // Reference: http://liangyenchen.blogspot.tw/2015/09/unity3d-debuglog.html
 **********************************************************/
-#if DebugFilter
+#if DEBUG_FILTER
 
 using System.Diagnostics;
 using UnityEngine;
 
 public static class Debug
 {
-    [Conditional("DebugFilter")]
+    [Conditional("DEBUG_FILTER")]
     public static void Break() { }
-    [Conditional("DebugFilter")]
+    [Conditional("DEBUG_FILTER")]
     public static void ClearDeveloperConsole() { }
-    [Conditional("DebugFilter")]
+    [Conditional("DEBUG_FILTER")]
     public static void DebugBreak() { }
-    [Conditional("DebugFilter")]
+
+    [Conditional("DEBUG_FILTER")]
     public static void DrawLine(Vector3 start, Vector3 end) { }
-    [Conditional("DebugFilter")]
+    [Conditional("DEBUG_FILTER")]
     public static void DrawLine(Vector3 start, Vector3 end, Color color) { }
-    [Conditional("DebugFilter")]
+    [Conditional("DEBUG_FILTER")]
     public static void DrawLine(Vector3 start, Vector3 end, Color color, float duration) { }
-    [Conditional("DebugFilter")]
+    [Conditional("DEBUG_FILTER")]
     public static void DrawLine(Vector3 start, Vector3 end, Color color, float duration, bool depthTest) { }
-    [Conditional("DebugFilter")]
+
+    [Conditional("DEBUG_FILTER")]
     public static void DrawRay(Vector3 start, Vector3 dir) { }
-    [Conditional("DebugFilter")]
+    [Conditional("DEBUG_FILTER")]
     public static void DrawRay(Vector3 start, Vector3 dir, Color color) { }
-    [Conditional("DebugFilter")]
+    [Conditional("DEBUG_FILTER")]
     public static void DrawRay(Vector3 start, Vector3 dir, Color color, float duration) { }
-    [Conditional("DebugFilter")]
+    [Conditional("DEBUG_FILTER")]
     public static void DrawRay(Vector3 start, Vector3 dir, Color color, float duration, bool depthTest) { }
 
-    [Conditional("DebugFilter")]
+    [Conditional("DEBUG_FILTER")]
     public static void Log(object message) { }
-    [Conditional("DebugFilter")]
-    public static void Log(object message, UnityEngine.Object context) { }
-    [Conditional("DebugFilter")]
-    public static void LogError(object message) { }
-    [Conditional("DebugFilter")]
-    public static void LogError(object message, UnityEngine.Object context) { }
-    [Conditional("DebugFilter")]
-    public static void LogException(System.Exception exception) { }
-    [Conditional("DebugFilter")]
-    public static void LogException(System.Exception exception, UnityEngine.Object context) { }
-    [Conditional("DebugFilter")]
+    [Conditional("DEBUG_FILTER")]
+    public static void Log(object message, Object context) { }
+    [Conditional("DEBUG_FILTER")]
+    public static void LogFormat(string format, params object[] args) { }
+    [Conditional("DEBUG_FILTER")]
+    public static void LogFormat(Object context, string format, params object[] args) { }
+
+    [Conditional("DEBUG_FILTER")]
     public static void LogWarning(object message) { }
-    [Conditional("DebugFilter")]
-    public static void LogWarning(object message, UnityEngine.Object context) { }
+    [Conditional("DEBUG_FILTER")]
+    public static void LogWarning(object message, Object context) { }
+    [Conditional("DEBUG_FILTER")]
+    public static void LogWarningFormat(string format, params object[] args) { }
+    [Conditional("DEBUG_FILTER")]
+    public static void LogWarningFormat(Object context, string format, params object[] args) { }
+
+    [Conditional("DEBUG_FILTER")]
+    public static void LogError(object message) { }
+    [Conditional("DEBUG_FILTER")]
+    public static void LogError(object message, Object context) { }
+    [Conditional("DEBUG_FILTER")]
+    public static void LogErrorFormat(string format, params object[] args) { }
+    [Conditional("DEBUG_FILTER")]
+    public static void LogErrorFormat(Object context, string format, params object[] args) { }
+
+    [Conditional("DEBUG_FILTER")]
+    public static void LogException(System.Exception exception) { }
+    [Conditional("DEBUG_FILTER")]
+    public static void LogException(System.Exception exception, Object context) { }
 }
 #endif
